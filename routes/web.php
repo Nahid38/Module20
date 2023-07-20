fd4b433b07ced07f29d5f4bf28f8ee90a0cba93f
Nahid Hossen <nahid957hossen@gmail.com>
2023-07-20 15:19:52 +0600

    module20

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenVerifyMiddleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::post('/user-registration', [UserController::class, 'UserRegistration']);
+Route::post('/user-login', [UserController::class, 'UserLogin']);
+Route::post('/send-otp', [UserController::class, 'SendOTP']);
+Route::post('/otp-verify', [UserController::class, 'VerifyOTP']);
+Route::post('/pass-reset', [UserController::class, 'PasswordReset'])->middleware(TokenVerifyMiddleware::class);
